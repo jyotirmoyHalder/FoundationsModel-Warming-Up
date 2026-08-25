5d2a90a78f51d81c3442dc2c0986aeaba84bf401
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-08-25 22:52:36 +0600

    Initial Commit

---
+++ FoundationsModel Warming Up/FoundationsModel_Warming_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationsModel_Warming_UpApp.swift
+//  FoundationsModel Warming Up
+//
+//  Created by jyotirmoy_halder on 25/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationsModel_Warming_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
